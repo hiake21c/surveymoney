e2a47af8c85c2cfcc848f8a2a97e644b3f162af3
Jongmin Kim <jongmin.kim@kakaogames-us.com>
2018-12-23 09:23:13 -0800

    init commit

---
+++ src/main/java/com/surveymoney/surveymoney/SurveymoneyApplication.java
@@ -0,0 +1,14 @@
+package com.surveymoney.surveymoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveymoneyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SurveymoneyApplication.class, args);
+    }
+
+}
+
