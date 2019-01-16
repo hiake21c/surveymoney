b247161649b0a77059758ff625df87cded6f64b3
EmilyPark <jjung4684@gmail.com>
2019-01-17 01:00:08 +0900

    Refactor : 엔티티 nullable 체크하는 어노테이션 변경

--- src/main/java/com/surveymoney/model/Member.java
+++ src/main/java/com/surveymoney/model/Member.java
@@ -1,11 +1,27 @@
 package com.surveymoney.model;
 
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "sv_member")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Member extends BaseModel{
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
+    @Size(min = 2, max = 10)
     private String memName;
 
+    @Column(nullable = false)
+    @Size(min = 3, max = 15)
     private String memId;
 
 
