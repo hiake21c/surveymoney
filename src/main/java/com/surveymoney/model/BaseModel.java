c8a1b1085080fc456c4615b5b7dc8cb05444db63
EmilyPark <jjung4684@gmail.com>
2019-02-04 17:28:38 +0900

    refactor : JPA columnDefinition type오류 수정
    LocalDateTime은 DB에 dateTime으로 들어가기 때문에 JPA의 엔티티 디폴드값을 같은 type으로 해야 한다.

--- src/main/java/com/surveymoney/model/BaseModel.java
+++ src/main/java/com/surveymoney/model/BaseModel.java
@@ -2,14 +2,10 @@ package com.surveymoney.model;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.ColumnDefault;
 
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 import java.time.LocalDateTime;
-import java.util.Date;
 
 @MappedSuperclass
 @Getter
@@ -19,13 +15,13 @@ public class BaseModel {
     @Column(nullable = false, columnDefinition="TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
     private LocalDateTime modifyDate;
 
-    @Column(nullable = false, columnDefinition="999")
+    @Column(nullable = false)
     private Long modifyId;
 
-    @Column(nullable = false, updatable=false, columnDefinition="TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    @Column(nullable = false, columnDefinition="TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
     private LocalDateTime createDate;
 
-    @Column(nullable = false, columnDefinition="999")
+    @Column(nullable = false)
     private Long createId;
 
 }
