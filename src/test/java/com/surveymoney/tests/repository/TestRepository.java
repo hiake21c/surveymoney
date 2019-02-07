fc7fcf0ad08c099e9099eeab6d38b03200316c40
EmilyPark <jjung4684@gmail.com>
2019-02-08 00:28:33 +0900

    test : Test 코드 삭제

--- src/test/java/com/surveymoney/tests/repository/TestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.surveymoney.tests.repository;
-
-import com.surveymoney.tests.model.Tests;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestRepository extends JpaRepository<Tests, Long> {
-}
