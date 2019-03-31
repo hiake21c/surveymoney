3824100666eb33dbd749a5c995ec7c880b7869cd
EmilyPark <jjung4684@gmail.com>
2019-03-31 18:18:44 +0900

    refactor : log4j 삽입

--- src/main/java/com/surveymoney/SurveymoneyApplication.java
+++ src/main/java/com/surveymoney/SurveymoneyApplication.java
@@ -1,12 +1,16 @@
 package com.surveymoney;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SurveymoneyApplication {
+    private static Logger logger = LogManager.getLogger(SurveymoneyApplication.class);
 
     public static void main(String[] args) {
+        logger.info("Starting Spring Boot SurveymoneyApplication");
         SpringApplication.run(SurveymoneyApplication.class, args);
     }
 
