be4041c05e06c0fcd82ca61d15c2f0c91c2cf0a8
EmilyPark <jjung4684@gmail.com>
2019-04-13 02:23:44 +0900

    feat : 검색조건의 도메인 객체 클래스 추가

---
+++ src/main/java/com/surveymoney/model/SurveyBaseSearch.java
@@ -0,0 +1,23 @@
+package com.surveymoney.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+public class SurveyBaseSearch {
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    LocalDateTime startDate;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    LocalDateTime endDate;
+
+    String title;
+
+}
